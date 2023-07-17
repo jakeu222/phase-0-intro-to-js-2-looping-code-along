816abb981d46ceea68a96e2b6af1bbdaf0af1290
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-07-17 16:34:08 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solutions in this file
+
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+
+}
+*/
+
+
+
+
+function writeCards(name, event) {
+    let array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+
+    return array
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
